15e17d7f7701e7a51c03b045f7c299b9a0bca568
DevNdamase <ndamase@tutanota.com>
2024-12-31 18:42:03 +0200

    feat: add solutions 07-10 of JavaScript exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  num = Number(num);
+  if (num < 0) {
+    return 'OOPS';
+  }
 
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+
+  let a = 0;
+  let b = 1;
+
+  for (let i = 2; i <= num; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return b;
 };
 
 // Do not edit below this line
